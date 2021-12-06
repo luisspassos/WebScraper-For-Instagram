74ee7204bbf3145440531567f955e36d6907b872
Luís Passos <luis.passos013@gmail.com>
2021-12-06 11:20:48 -0300

    added spread of images and videos

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.instagram.com/reel/CXBlhblDF_f/');
-    await page.evaluate(()=> {
-        const media = document.querySelector("video");
-        const author = document.querySelector("a");
+require('dotenv').config();
+
+function FetchInstagram(link) {
+    return new Promise(async function(resolve, reject) {
+        try {
+            const ref = link.split('/').filter(el => el !== "").pop();
+
+            const browser = await puppeteer.launch({
+                headless: false,
+            });
+            const page = await browser.newPage();
+            await page.goto(`https://imginn.org/p/${ref}/`);
         
-        console.log(media)
-        console.log(author)
+            await page.evaluate(() => {
+
+                window.scrollTo(0, window.innerHeight)
+
+                const post = {
+                    authorImg: document.querySelector(".avatar > img").src,
+                    authorNick: document.querySelector(".name").textContent.split(" ")[0],
+                    authorNickname: document.querySelector(".nickname").textContent,
+                    date: document.querySelector(".date").textContent,
+                    desc: document.querySelector(".desc").textContent,
+                    media: [...document.querySelectorAll(".content img"), ...document.querySelectorAll(".content video")].map(({src})=> src)
+                }
+
+                console.log(post);
+
+                //await browser.close();
+            })
+            resolve();
+        } catch (e) {
+            reject(e);
+        }   
+    
     })
+    
+};
 
-
-    //await browser.close();
-})();
+(async () => { await FetchInstagram('https://www.instagram.com/p/CWoP3rhPkKM/');})()
+console.log("AAAAAAAAAAAAAAAAAAAA")
